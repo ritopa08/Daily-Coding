553719fad4c823cb22cef9a0f5f60903beb5caa1
Poosstoo <pieu91@gmail.com>
2018-12-03 14:02:55 +0530

    Add files via upload

---
+++ P8.py
@@ -0,0 +1,35 @@
+'''
+Implement an autocomplete system. That is, given a query string s and a set of all possible query strings,
+ return all strings in the set that have s as a prefix.
+
+For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+
+Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries
+
+'''
+
+def mat(p,s):
+  m=len(p)
+  d=len(s)
+  t=[]
+  q=""
+  for i in range (d):
+    x=s[i]
+    for k in range(m):
+      if(p[k]==x[k]) : 
+        q +=x[k] 
+      else:
+        q= " " 
+    if(len(q)>=m):
+      t.append(x)
+        
+  print(t)      
+
+    
+
+
+s=['road','roar','root','roap']
+p='roa'  
+if(type(p)==list):
+  p=" ".join(map(str,p))   
+mat(p,s)
