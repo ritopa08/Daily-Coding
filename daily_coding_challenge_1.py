da1d1f7f5519782e9f7998120468373ee55dc733
Poosstoo <pieu91@gmail.com>
2018-12-17 14:17:58 +0530

    Update daily_coding_challenge_1.py

--- daily_coding_challenge_1.py
+++ daily_coding_challenge_1.py
@@ -6,19 +6,35 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 '''
 
+#defining a function
 def match_list(a,k):
-  l=[]
+  #Initializes empty list
+  l=[]       
+  
+  
+  #calculate the sum of two numbers in the list
   for i in range(len(a)):
     for j in range(i+1,len(a)):
       s=a[i]+a[j]
-      l.append(s)
+      
+      #add each sum to the list
+      l.append(s)    
+      
+      
+  #Remove duplicates from the list    
   l=set(l)
+  
+  #checking whether element is present in the list
   if k in (l):
     print("true")
   else:
     print("false")        
 
+    
+    
+#taking inputs
+a=[10, 15, 3, 7]   #array containing numbers
+k=13               #element to be searched
 
-a=[10, 15, 3, 7]
-k=13
-match_list(a,k)
+#calling function
+match_list(a,k)    
